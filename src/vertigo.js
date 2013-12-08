78c2864898af870ffe73e0f8aaab7d7d6e2dcbb7
Jeremy Kahn <jeremyckahn@gmail.com>
2013-12-08 15:22:51 -0600

    Remove some boilerplate.

--- src/vertigo.js
+++ src/vertigo.js
@@ -1,38 +1,18 @@
 /*
  * vertigo
- * 
+ *
  *
  * Copyright (c) 2013 Jeremy Kahn
  * Licensed under the MIT license.
  */
 
-(function ($) {
+;(function ($) {
+  'use strict';
 
-  // Collection method.
-  $.fn.awesome = function () {
+  $.fn.vertigo = function () {
     return this.each(function (i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+      var $el = $(this);
     });
-  };
-
-  // Static method.
-  $.awesome = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
-
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
-
-  // Custom selector.
-  $.expr[':'].awesome = function (elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
   };
 
 }(jQuery));

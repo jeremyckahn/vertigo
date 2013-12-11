99188ac76cf69fdc130e3315bed02cb0dd892131
Jeremy Kahn <jeremyckahn@gmail.com>
2013-12-10 21:15:20 -0600

    Adds sample for AMD loading via RequireJS, fixes AMD dependency listing.

--- src/vertigo.js
+++ src/vertigo.js
@@ -259,7 +259,7 @@
 
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['jquery'], initVertigo);
+    define(['jquery', 'jquery-mousewheel'], initVertigo);
   } else if (typeof exports === 'object') {
     // Node/CommonJS style for Browserify
     module.exports = initVertigo;
